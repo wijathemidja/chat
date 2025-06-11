a0dae7df9d1f989cd63157a51b134802b76eb72c
wija <167352879+wijathemidja@users.noreply.github.com>
2025-06-11 21:02:17 +0100

    Add files via upload

---
+++ assets/status-tap-CJm7An_K.js
@@ -0,0 +1,3 @@
+import{B as a,C as i,D as c,E as d,G as l}from"./index-CZv07G6e.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{a(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(r=>c(t,r)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
